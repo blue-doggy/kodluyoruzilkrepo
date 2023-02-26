4d5fc2abc91d8e2248670d4f736720034b075336
blue-doggy <muhammetdogancay@gmail.com>
2023-02-26 23:51:51 +0300

    23

---
+++ java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("ebenin amı");
+        byte vByte = 100;
+        System.out.println(vByte);
+        short vShort = 1000 ;
+        int vInt = 12800 ;
+    System.out.println(vByte *  vShort);
+    int vBok = vShort / vByte;
+            System.out.println(vInt / vBok);
+            long vLong = 128000;
+                    System.out.println(vLong / vInt);
+                    float vFloat = 10.9f;
+                double vDouble = 109.0;
+                char vChar = 'b';
+         vChar = 'c';
+        System.out.println(vChar);
+        System.out.println(vDouble / vFloat);
+        char v2Char = 240;
+        System.out.println(v2Char);
+
+        char c1 = 'a';
+        char c2 = 'j';
+        char c3 = 'v';
+        System.out.println("" + c2 + c1 + c3 + c1);
+    }
+}
--- java/java.java
+++
@@ -1,4 +0,0 @@
-public static void main(String[] args) {
-    String yağmur = "fıstık" ;
-    System.out.println(yağmur);
-}
